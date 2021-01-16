b5d811c2c13cf177412e6616bbd5940282192a07
VIctor J. Ramos <victorjramos2@gmail.com>
2021-01-15 18:58:10 -0800

    Update diffrent_newimage.py

--- diffrent_newimage.py
+++ diffrent_newimage.py
@@ -1,3 +1,6 @@
 newimage[x][y] = (int(image[x-1][y-1])*int(image[x-1][y])*int(image[x-1][y+1])*int(image[x][y-1]) *
                   int(image[x][y])*int(image[x][y+1])*int(image[x+1][y-1])*int(image[x+1][y]) *
                   int(image[x+1][y+1]))
+
+newimage[x][y] = (image[x-1][y-1]*image[x-1][y]*image[x-1][y+1]*image[x][y-1]*image[x][y]*image[x][y+1] *
+                  image[x+1][y-1]*image[x+1][y]*image[x+1][y+1])
